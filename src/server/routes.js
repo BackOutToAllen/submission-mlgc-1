167feaa2853235b96578c52684eb8ef803169ea5
Richard Soegito <richardsoegito99@gmail.com>
2024-12-11 11:40:35 +0700

    first commit

---
+++ src/server/routes.js
@@ -0,0 +1,22 @@
+const {postPredictHandler, postHistoriesHandler} = require('../server/handler');
+ 
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options:{
+        payload:{
+            allow: 'multipart/form-data',
+            multipart: true
+        }
+    }
+  },
+  {
+    path: '/predict/histories',
+    method:'GET',
+    handler: postHistoriesHandler
+  }
+]
+ 
+module.exports = routes;
